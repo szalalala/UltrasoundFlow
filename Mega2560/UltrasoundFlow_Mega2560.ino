41e7570d912aa2f6a4f65d8ae00e62925420facd
szalalala <szalalala@gmail.com>
2020-01-28 14:35:13 +0100

    Secound commit, comments

--- Mega2560/UltrasoundFlow_Mega2560.ino
+++ Mega2560/UltrasoundFlow_Mega2560.ino
@@ -17,32 +17,32 @@ void setup() {
   pinMode(N1, OUTPUT);
   pinMode(N2, OUTPUT);
 
-  /*Timer 4*/
+  /*Timer 4 Input Capture Unit*/
   TCCR4A = B00000000;
   TCCR4B =  B01000000; //ICES4
   TCCR4C = B00000000;
   TIFR4 = TIFR4 | B00100000; //ICF4
   TIMSK4 = TIMSK4 | B00100000; //ICIE4
-  /*Timer 5*/
+  /*Timer 5 Input Capture Unit*/
   TCCR5A = B00000000;
   TCCR5B =  B01000000; //ICES5
   TCCR5C = B00000000;
   TIFR5 = TIFR5 | B00100000; //ICF5
   TIMSK5 = TIMSK5 | B00100000; //ICIE5
-  /*Timer 1 - CTC OCR1A*/
+  /*Timer 1 - CTC OCR1A Clear Timer on Compare Match*/
   TCCR1A = B00000000; 
   TCCR1B = B00001100; //WGM12,CS12,CS11,CS10,prescal_256
   TCCR1C = B00000000;
   TIFR1 = TIFR1 | B00000010;//OCF1A
   TIMSK1 = TIMSK1 | B00000010;//OCIE1A
-  OCR1A = 15000;
-  /*Timer 3 - CTC OCR3A*/
+  OCR1A = 15000; // Every 240 ms interup
+  /*Timer 3 - CTC OCR3A Clear Timer on Compare Match*/
   TCCR3A = B00000000; 
   TCCR3B = B00001100; //WGM32,CS32,CS31,CS30,prescal_256
   TCCR3C = B00000000;
   TIFR3 = TIFR3 | B00000010;//OCF3A
   TIMSK3 = TIMSK3 | B00000010;//OCIE3A
-  OCR3A = 15000;
+  OCR3A = 15000; // Every 240 ms interup)
   /**/
   TCNT4 = 0;
   TCNT1 = 0;
